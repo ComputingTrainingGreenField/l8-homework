d5389ad383d8160cf0ce05dcbe15c23a06e85bf2
John Zhang <45115057+John1798@users.noreply.github.com>
2018-12-09 14:46:42 +0800

    Update browse.component.ts

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -6,11 +6,18 @@ import { Component, OnInit } from "@angular/core";
     templateUrl: "./browse.component.html"
 })
 export class BrowseComponent implements OnInit {
+    icon_x = 50;
+    icon_y = 50;
+    text_x = 100;
+    text_y = 100;
+    label_x = 50;
+    label_y = 200;
+    count = 0; 
+
     constructor() {
         // Use the component constructor to inject providers.
     }
-
-    ngOnInit(): void {
+    ngOnInit(): void {
         // Use the "ngOnInit" handler to initialize data for the view.
     }
 }
