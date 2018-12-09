e36338cf0ffa78e2be33c44e29e4dcdbaadc84aa
John Zhang <45115057+John1798@users.noreply.github.com>
2018-12-09 15:08:00 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptFormsModule } from "nativescript-angular/forms"
 
 import { AppRoutingModule, COMPONENTS } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
     bootstrap: [
@@ -10,8 +12,11 @@ import { AppComponent } from "./app.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        NativeScriptFormsModule,
+        AppRoutingModule,
+        CoreModule
     ],
+
     declarations: [
         AppComponent,
         ...COMPONENTS
